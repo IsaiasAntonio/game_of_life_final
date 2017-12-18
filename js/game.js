bc238a4c01f1b53298081742ca91e2b1350697db
Isaias <isaororio@gmail.com>
2017-12-18 02:04:08 -0600

    fixing syntax and testing

--- js/game.js
+++ js/game.js
@@ -1,21 +1,7 @@
-let longValue="50" , Timer, flagAux , speed=1000;
+let longValue="50", timer, flagAux, speed=1000;
 
-function start() {
-    Timer = setInterval(nextToChange, speed);            
-}
-
-function stop() {
-    clearTimeout(Timer);                      
-}
-function nextToChange(){
-	startMatrix();
-	for(let i=0;i<longValue;i++)
-		for(let j=0;j<longValue;j++)
-		{
-			cellStatus(i,j);					
-		}
-	changeStatus();
-}
+startMatrix();
+dTableValues();
 
 function startMatrix()
 {
@@ -24,42 +10,13 @@ function startMatrix()
 		flagAux[a]=new Array(longValue);
 }	
 
-function fillValues(){	
-	for(let i=0;i<longValue;i++){
-        for(let j=0;j<longValue;j++)
-		{
-			document.getElementById(i+","+j).className = Math.random() > 0.7 ? 'alive' : 'dead';
-		}
-    }
-		
+function changeStatus(){
+	for(let i=0;i<longValue;i++)
+		for(let j=0;j<longValue;j++)
+			document.getElementById(i+","+j).className=flagAux[i][j];	
 }
 
-function cellStatus(i,j){
-	let result=cellEnviroment(i,j);
-	if(document.getElementById(i+","+j).className=="dead" && result==3){
-        flagAux[i][j]="alive";
-    }
-		
-	else if(document.getElementById(i+","+j).className=="alive" && (result==2 || result==3)){
-        flagAux[i][j]="alive";
-    }
-		
-	else{
-        flagAux[i][j]="dead";
-    }
-			
-}
 
-function dTableValues(){
-    document.getElementById("indexTable").innerHTML="";
-for(let i=0;i<longValue;i++){
-    document.getElementById("indexTable").innerHTML+="<tr id="+i+"></tr>";
-    for(let j=0;j<longValue;j++){
-        document.getElementById(i).innerHTML+="<td class='dead' id="+i+","+j+"></td> ";
-    }
-      
-}
-}
 
 function cellEnviroment(aux,aux2)
 {
@@ -113,4 +70,54 @@ function cellEnviroment(aux,aux2)
     }
 		
 	return life;
+}
+
+
+function stop() {
+    clearTimeout(timer);                      
+}
+
+
+function dTableValues(){
+		document.getElementById("indexTable").innerHTML="";
+	for(let i=0;i<longValue;i++){
+		document.getElementById("indexTable").innerHTML+="<tr id="+i+"></tr>";
+		for(let j=0;j<longValue;j++)
+			document.getElementById(i).innerHTML+="<td class='dead' id="+i+","+j+"></td> ";
+	}
+}
+
+
+function cellStatus(i,j){
+	let result=cellEnviroment(i,j);
+	if(document.getElementById(i+","+j).className=="dead" && result==3)
+		flagAux[i][j]="alive";
+	else if(document.getElementById(i+","+j).className=="alive" && (result==2 || result==3))
+		flagAux[i][j]="alive";
+	else
+		flagAux[i][j]="dead";	
+}
+
+
+function start() {
+    timer = setInterval(nextToChange, speed);            
+}
+
+function fillValues(){	
+	for(let i=0;i<longValue;i++)
+		for(let j=0;j<longValue;j++)
+		{
+			document.getElementById(i+","+j).className = Math.random() > 0.7 ? 'alive' : 'dead';
+		}
+}
+
+
+function nextToChange(){
+	startMatrix();
+	for(let i=0;i<longValue;i++)
+		for(let j=0;j<longValue;j++)
+		{
+			cellStatus(i,j);					
+		}
+	changeStatus();
 }

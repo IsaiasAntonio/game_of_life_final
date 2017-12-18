6d194f85a321614718054845ac28f1c8cdcb1972
Isaias <isaororio@gmail.com>
2017-12-18 01:35:01 -0600

    adding functions like evualuations of the cell enviroment, and css styles

--- js/game.js
+++ js/game.js
@@ -25,27 +25,35 @@ function startMatrix()
 }	
 
 function fillValues(){	
-	for(let i=0;i<longValue;i++)
-		for(let j=0;j<longValue;j++)
+	for(let i=0;i<longValue;i++){
+        for(let j=0;j<longValue;j++)
 		{
 			document.getElementById(i+","+j).className = Math.random() > 0.7 ? 'alive' : 'dead';
 		}
+    }
+		
 }
 
 function cellStatus(i,j){
 	let result=cellEnviroment(i,j);
-	if(document.getElementById(i+","+j).className=="dead" && result==3)
-		flagAux[i][j]="alive";
-	else if(document.getElementById(i+","+j).className=="alive" && (result==2 || result==3))
-		flagAux[i][j]="alive";
-	else
-		flagAux[i][j]="dead";	
+	if(document.getElementById(i+","+j).className=="dead" && result==3){
+        flagAux[i][j]="alive";
+    }
+		
+	else if(document.getElementById(i+","+j).className=="alive" && (result==2 || result==3)){
+        flagAux[i][j]="alive";
+    }
+		
+	else{
+        flagAux[i][j]="dead";
+    }
+			
 }
 
 function dTableValues(){
-    document.getElementById("tablacont").innerHTML="";
+    document.getElementById("indexTable").innerHTML="";
 for(let i=0;i<longValue;i++){
-    document.getElementById("tablacont").innerHTML+="<tr id="+i+"></tr>";
+    document.getElementById("indexTable").innerHTML+="<tr id="+i+"></tr>";
     for(let j=0;j<longValue;j++){
         document.getElementById(i).innerHTML+="<td class='dead' id="+i+","+j+"></td> ";
     }
@@ -53,3 +61,56 @@ for(let i=0;i<longValue;i++){
 }
 }
 
+function cellEnviroment(aux,aux2)
+{
+	let life=0;
+	if(aux>=1 ){
+        if(document.getElementById((aux-1)+","+aux2).className == "alive"){
+            life++;
+        }
+    }		
+	if(aux<longValue-1){
+        if(document.getElementById((aux+1)+","+aux2).className == "alive"){
+            life++;
+        }
+        
+    }	
+	if(aux2<longValue-1){
+        if(document.getElementById(aux+","+(aux2+1)).className == "alive"){
+            life++;
+        }
+        
+    }	
+	if(aux2>=1){
+        if(document.getElementById(aux+","+(aux2-1)).className == "alive"){
+            life++;
+        }
+        
+    }
+	if(aux>=1 && aux2<longValue-1){
+        if(document.getElementById((aux-1)+","+(aux2+1)).className == "alive"){
+            life++;
+        }
+        
+    }	
+	if(aux>=1 && aux2!=0){
+        if(document.getElementById((aux-1)+","+(aux2-1)).className == "alive"){
+            life++;
+        }
+        
+    }	
+	if(aux<longValue-1 && aux2<longValue-1){
+        if(document.getElementById((aux+1)+","+(aux2+1)).className == "alive"){
+            life++;
+        }
+        
+    }	
+	if(aux<longValue-1 && aux2>0){
+        if(document.getElementById((aux+1)+","+(aux2-1)).className == "alive"){
+            life++;
+        }
+        
+    }
+		
+	return life;
+}
